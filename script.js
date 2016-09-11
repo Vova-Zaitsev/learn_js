bcacd117ccf12635e5f15e52a8759ae89c281509
Vova-Zaitsev <11colgate11@gmail.com>
2016-09-11 03:34:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-for(var i=1; i<=7; ++i)
-{
-    for (var j=1; j<=i; j++)
+var users = [
     {
- 
-        console.log("#");
+        firstName: 'Dmitry',
+        lastName: 'Kozlov',
+        birthDay: '16.06.1990',
+        phones: [
+            '89001112233',
+            '83458962574'
+        ]
+    },
+    {
+        firstName: 'Ivan',
+        lastName: 'Petrov',
+        birthDay: '08.05.1992',
+        phones: [
+            '89990003344',
+            '84456763883'
+        ]
     }
-   
-}
-console.log("\n");
+];
+
+var user = {};
+
+user.firstName = prompt('Ввведите имя:');
+user.lastName = prompt('Ввведите фамилию:');
+user.birthDay = prompt('Введите дату рождения:');
+user.phones = [];
+user.phones[0] = prompt('Введите номер телефона:');
+
+users.push(user);
+
+var i;
+for (i = 0; i < users.lenght; i = i + 1) {console.log('Имя:' + users[i].firstName, +'Фамилия:' + users[i].lastName, +'Дата рождения:' + users[i].birthDay, +'Номер телефона:' + users[i].phones.join(', ')); }
